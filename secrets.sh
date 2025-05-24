79ec8360952ee492eb13040997b14b9e31bbe9fe
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-05-24 11:27:57 +0100

    add:echoSecrets

--- secrets.sh
+++ secrets.sh
@@ -2,3 +2,5 @@
 export SECRET_KEY='${{ secrets.SECRET_KEY }}'
 export DB_USER='${{ secrets.DB_USER }}'
 export DB_PASSWORD='${{ secrets.DB_PASSWORD }}'
+
+echo "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}'; " | psql
