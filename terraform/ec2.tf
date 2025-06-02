d4e68fba550479718a5bc1838c5b770f1707e703
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-06-02 18:40:52 +0100

    add:AMI add

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -22,3 +22,24 @@ resource "aws_security_group" "allow_http" {
     vpc_id      = data.terraform_remote_state.source.outputs.vpc_id
 }
 
+data "aws_ami" "CT_AMI" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^myami-[0-9]{3}"
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
