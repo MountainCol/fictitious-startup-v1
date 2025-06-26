4b64802e72c1e396a766534afbdce502d314c2ba
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-06-26 17:05:02 +0100

    chore:change

--- terraform/custom_ami_version.tf
+++ terraform/custom_ami_version.tf
@@ -1,12 +1,12 @@
 data "aws_ami" "" {
   executable_users = ["self"]
   most_recent      = true
-  name_regex       = "^myami-[0-9]{3}"
+  name_regex       = "CloudTalents"
   owners           = ["self"]
 
   filter {
     name   = "name"
-    values = ["var.custom_ami_id"]
+    values = ["cloudtalents-startup-latest"]
   }
 
   filter {
